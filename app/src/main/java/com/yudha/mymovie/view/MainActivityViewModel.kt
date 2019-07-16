a0cd7abef26d73c12301d66cf251ec9ff8e5fdef
yosefbrian <yosefbrian@gmail.com>
2019-07-16 10:41:09 +0700

    create view for genre

---
+++ app/src/main/java/com/yudha/mymovie/view/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.yudha.mymovie.view
+
+/**
+ * Created by yudha on 16,July,2019
+ */
+class MainActivityViewModel {
+}
---
+++ app/src/main/java/com/yudha/mymovie/view/movie/GenreAdapter.kt
@@ -0,0 +1,7 @@
+package com.yudha.mymovie.view.movie
+
+/**
+ * Created by yudha on 16,July,2019
+ */
+class GenreAdapter {
+}
